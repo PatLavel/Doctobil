3b768a00e60d0cf7147ecfc7add113566db39c3c
lavelle <p.lavelle@numericable.fr>
2021-06-17 11:05:11 +0200

    new fixtures

--- src/DataFixtures/RdvFixtures.php
+++ src/DataFixtures/RdvFixtures.php
@@ -13,30 +13,30 @@ class RdvFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-            $tab=["a","b","c","d","e"];
+            $tab=["a","b","c","d","e","f"];
         for ($i = 1; $i < 6; $i++) {
             $patient = new Patient();
             $ddn = new DateTime;
             $ddn->setDate(1978, 06, 07);
-            $patient->setNom("Patient $tab[$i]")
-                ->setPrenom("Jean $tab[$i]")
+            $patient->setNom("Patient$tab[$i]")
+                ->setPrenom("Jean$tab[$i]")
                 ->setNoSecu(123456789012345)
                 ->setAdresse("4$i rue du truc")
-                ->setVille("Ville $tab[$i]")
+                ->setVille("Ville$tab[$i]")
                 ->setTelephone("023565958$i")
-                ->setMail("mail $tab[$i]@mail.com")
+                ->setMail("mail$tab[$i]@mail.com")
                 ->setSexe("Homme")
                 ->setDdn($ddn);
             $manager->persist($patient);
 
             $doc = new Docteur();
-            $doc->setNom("Docteur $tab[$i]")
-                ->setPrenom("Jean $tab[$i]")
+            $doc->setNom("Docteur$tab[$i]")
+                ->setPrenom("Jean$tab[$i]")
                 ->setAdresse("5$i rue du truc")
-                ->setVille("Ville $tab[$i]")
+                ->setVille("Ville$tab[$i]")
                 ->setSpecialite("Neurologie")
                 ->setTelephone("023565958$i")
-                ->setMail("mail $tab[$i]@mail.com");
+                ->setMail("mail$tab[$i]@mail.com");
             $manager->persist($doc);
 
             $rdv = new Rdv();
