c3be1ec7d9d6a8455cf693499da2109254945a07
Denis LRX <rotenfinger@gmail.com>
2021-06-17 11:12:42 +0200

    test RdvRepository

--- tests/Repository/RdvRepositoryTest.php
+++ tests/Repository/RdvRepositoryTest.php
@@ -13,13 +13,43 @@ class RdvRepositoryTest extends KernelTestCase
 
     use FixturesTrait;
 
+
+
     public function testFindAll()
     {
         self::bootKernel();
         $repository = self::$container->get(RdvRepository::class);
         $this->loadFixtures([RdvFixtures::class]);
-        $patients = $repository->findAll();
-        $this->assertCount(5, $patients);
+        $rdv = $repository->findAll();
+        $this->assertCount(5, $rdv);
     }
 
+    public function testFindBy()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $this->loadFixtures([RdvFixtures::class]);
+        $rdv = $repository->findBy(['date' => '2021-05-05']);
+        $this->assertCount(1, $rdv);
+    }
+
+    public function testFind()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $this->loadFixtures([RdvFixtures::class]);
+        $rdv = $repository->find();
+        $this->assertCount(1, $rdv);
+    }
+
+    public function testFindOneBy()
+    {
+        self::bootKernel();
+        $repository = self::$container->get(RdvRepository::class);
+        $a = $repository->findAll();
+        $id = $a[0]->getId();
+        $this->loadFixtures([RdvFixtures::class]);
+        $rdv = $repository->findOneBy($id);
+        $this->assertCount($id, $rdv->getId());
+    }
 }
